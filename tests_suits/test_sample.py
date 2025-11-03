d55af01c5ca2cc725c32a9ef97c9e779f71ecc12
rBogdanCS <rosca.bogdan02@gmail.com>
2025-11-03 19:08:34 +0100

    fix flake8

--- tests_suits/test_sample.py
+++ tests_suits/test_sample.py
@@ -2,18 +2,21 @@ def test_failing(config):
     """Failing test to validate rerun"""
     assert False
 
+
 def test_config(config):
-    """Simple test to validate loading of configuration through conftest fixture"""
+    """Check loading of configuration through conftest fixture"""
     assert config["base_url"] == "https://m.twitch.tv"
+
 
 def test_open_page(driver, config):
     """Simple test to open the base URL and verify page loads"""
     driver.get(config["base_url"])
-    
+
     assert driver.title != ""
-    
+
     current_url = driver.current_url
     assert current_url == config["base_url"] + "/"
+
 
 def test_youtube(driver):
     driver.get('https://www.youtube.com/')
